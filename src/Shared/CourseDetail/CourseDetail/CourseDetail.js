205547f3c82965fdd321ee2cf9cb801d13969842
S0vers <sovers.pandey@outlook.com>
2022-10-27 14:45:28 +0600

    fixed printing issues

--- src/Shared/CourseDetail/CourseDetail/CourseDetail.js
+++ src/Shared/CourseDetail/CourseDetail/CourseDetail.js
@@ -3,20 +3,14 @@ import { Link, useLoaderData } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { FaRegEye, FaStar, FaFilePdf } from 'react-icons/fa';
-import { useReactToPrint } from 'react-to-print';
-import toast from 'react-hot-toast';
+import ReactPrint from 'react-to-print';
 const CourseDetail = () => {
     const details = useLoaderData();
     const { course_id, Rating, detail, picture, time, title, totalview } = details[0]
-    const componentRef = useRef();
-    const handlePrint = useReactToPrint({
-        content: () => componentRef.current,
-        documentTitle: `Content Details ${course_id}`,
-        onAfterPrint: () => toast.success('Print successful')
-    })
+    const ref = useRef();
     return (
         <div className='mt-4 col d-flex justify-content-center'>
-            <Card ref={componentRef} className='' style={{ width: '64rem' }}>
+            <Card className='' style={{ width: '64rem' }}>
                 <Card.Img style={{ height: '20rem' }} variant="top" src={picture} />
                 <Card.Body>
                     <Card.Header className='d-flex justify-content-between align-items-center'>
@@ -26,7 +20,7 @@ const CourseDetail = () => {
                             </div>
                         </div>
                         <div>
-                            <FaFilePdf onClick={handlePrint} className='me-2'></FaFilePdf>
+                            <ReactPrint trigger={() => <FaFilePdf className='me-2'></FaFilePdf>} content={() => ref.current} />
                         </div>
                     </Card.Header>
                     <div className="d-flex justify-content-between mb-4">
@@ -51,7 +45,34 @@ const CourseDetail = () => {
                     <Link to={`/checkout/${course_id}`}><Button style={{ textDecoration: 'none', backgroundColor: '#5bbcfc', border: 'none' }} hover='' variant="primary">Get Premium access</Button></Link>
                 </Card.Body>
             </Card >
-        </div>
+            <div className='d-none'>
+                <Card ref={ref}>
+                    <Card.Img variant="top" src={picture} />
+                    <Card.Body>
+                        <Card.Title>{title}</Card.Title>
+                        <div className="d-flex justify-content-between mb-4">
+                            <div>
+                                <FaRegEye className='me-2'></FaRegEye>
+                                <span>{time}</span>
+                            </div>
+                            <div>
+                                <FaStar className='text-warning me-2'></FaStar>
+                                <span>{Rating}</span>
+                            </div>
+                            <div>
+                                <FaRegEye className='me-2'></FaRegEye>
+                                <span>{totalview}</span>
+                            </div>
+                        </div>
+                        <Card.Text >
+                            <>
+                                {detail}
+                            </>
+                        </Card.Text>
+                    </Card.Body>
+                </Card >
+            </div>
+        </div >
     );
 };
 
