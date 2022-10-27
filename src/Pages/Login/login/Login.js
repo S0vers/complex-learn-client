139edeb876f9d53a7a1f15c42259fc9c7eefb203
S0vers <sovers.pandey@outlook.com>
2022-10-27 11:27:12 +0600

    login complete

--- src/Pages/Login/login/Login.js
+++ src/Pages/Login/login/Login.js
@@ -66,12 +66,12 @@ const Login = () => {
                     <Form onSubmit={handleSubmit}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" required />
+                            <Form.Control name='email' type="email" placeholder="Enter email" required />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" required />
+                            <Form.Control name='password' type="password" placeholder="Password" required />
                             <Form.Text className="text-muted">
                                 New to the website? <Link to='/register'>Register Now</Link>
                             </Form.Text>
