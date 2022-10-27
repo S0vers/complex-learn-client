d87e2ba6109139e0b38e0dce054e21a3ffc6aad2
S0vers <sovers.pandey@outlook.com>
2022-10-27 15:17:16 +0600

    fixed header image sizing

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,51 +1,63 @@
-import React, { useRef, useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import "swiper/css";
-import "swiper/css/navigation";
-import './Home.css'
+import home1 from '../../../assets/carouselImage/carasoul1.jpg';
+import home2 from '../../../assets/carouselImage/carasoul2.jpg';
+import home3 from '../../../assets/carouselImage/carasoul3.jpg';
+import home4 from '../../../assets/carouselImage/carasoul4.jpg';
 const Home = () => {
     return (
-        <div className='container'>
-            <Carousel fade>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100 h-100"
-                        src="https://www.itprotoday.com/sites/itprotoday.com/files/programming.jpg"
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="https://res.cloudinary.com/grand-canyon-university/image/fetch/w_750,h_564,c_fill,g_faces,q_auto/https://www.gcu.edu/sites/default/files/2020-09/programming.jpg"
-                        alt="Second slide"
-                    />
+        <Carousel>
+            <Carousel.Item className='Carousel'>
+                <img
+                    className="d-block w-100"
+                    src={home1}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>Start Your Own career</h3>
+                    <p>Join us and start your own career in programming.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className='Carousel'>
+                <img
+                    className="d-block w-100"
+                    src={home2}
+                    alt="Second slide"
+                />
 
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="holder.js/800x400?text=Third slide&bg=20232a"
-                        alt="Third slide"
-                    />
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className='Carousel'>
+                <img
+                    className="d-block w-100"
+                    src={home3}
+                    alt="Third slide"
+                />
 
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                        </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-        </div>
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className='Carousel'>
+                <img
+                    className="d-block w-100"
+                    src={home4}
+                    alt="Fourth Slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     );
 };
 
