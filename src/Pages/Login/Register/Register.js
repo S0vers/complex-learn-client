dc2427880d7b49b544ab4a2cd6b82125ad509c15
S0vers <sovers.pandey@outlook.com>
2022-10-27 12:31:16 +0600

    register complete

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,21 +1,62 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
-import { Link } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../Contexts/AuthProvider/AuthProvider';
 const Register = () => {
+    const [error, setError] = useState('')
+    const navigate = useNavigate();
+    const { createUser, updateUserProfile, verifyEmail } = useContext(AuthContext);
+    const handleSubmit = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photoURL = form.photoURL.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        createUser(email, password)
+            .then(result => {
+                const user = result.user
+                console.log(user);
+                form.reset();
+                handleUpdateUserProfile(name, photoURL);
+                handleEmailVerification();
+                toast.success('Please verify your email address.')
+                setError('')
+                navigate('/')
+            })
+            .catch(e => {
+                setError(e.message);
+            })
+    }
+    const handleUpdateUserProfile = (name, photoURL) => {
+        const profile = {
+            displayName: name,
+            photoURL: photoURL
+        }
+        updateUserProfile(profile)
+            .then(() => { })
+            .catch(e => console.error(e));
+    }
+    const handleEmailVerification = () => {
+        verifyEmail()
+            .then(() => { })
+            .catch(e => console.error(e))
+    }
     return (
         <div className='mt-4 col d-flex justify-content-center'>
             <Card style={{ width: '36rem' }}>
                 <Card.Body>
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group className="mb-3" controlId="formBasicName">
                             <Form.Label>Name</Form.Label>
-                            <Form.Control name='name' type="email" placeholder="Your Name" />
+                            <Form.Control name='name' type="text" placeholder="Your Name" />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicPhoto">
                             <Form.Label>PhotoURL</Form.Label>
-                            <Form.Control name='PhotoURL' type="email" placeholder="Enter email" />
+                            <Form.Control name='photoURL' type="text" placeholder="Photo URL" />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
@@ -32,6 +73,9 @@ const Register = () => {
                         <Button variant="primary" style={{ textDecoration: 'none', backgroundColor: '#5bbcfc', border: 'none' }} type="submit">
                             Register
                         </Button>
+                        <Form.Text className="text-danger">
+                            {error}
+                        </Form.Text>
                     </Form>
                 </Card.Body>
             </Card>
